8ea6e34445bad238a0778b6cb0aa3713695f2793
Ignacio Saez <ignaciosaezreyes3@gmail.com>
2022-12-22 21:30:11 -0300

    adding new weapon shoot

--- rigth2death/main.py
+++ rigth2death/main.py
@@ -1,11 +1,11 @@
 import pygame
-from pygame.constants import K_LEFT, K_RIGHT, K_UP, K_DOWN, K_SPACE
+from pygame.constants import K_LEFT, K_RIGHT, K_UP, K_DOWN, K_SPACE, K_LCTRL
 
 from scenarios.stages import Stage
 
 
 def run():
-    allowed_moves = [K_LEFT, K_RIGHT, K_UP, K_DOWN, K_SPACE,K_LCTRL]
+    allowed_moves = [K_LEFT, K_RIGHT, K_UP, K_DOWN, K_SPACE, K_LCTRL]
     pygame.init()
     stage = Stage(allowed_moves)
     stage.run()

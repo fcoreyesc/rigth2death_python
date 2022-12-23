6efb6aeaa0f3b99bfa9c1f699c618b80e328bed7
Francisco Reyes <fco.reyes.c@gmail.com>
2022-12-23 01:23:10 +0100

    adding new zombie animation and create more classes (#24)
    * adding new zombie animation and create more classes
    
    * refactor of sprites
    
    * - refactor main.py
    - fixing zombie initial position
    
    * - adding new zombie

--- rigth2death/characters/enemies/zombies.py
+++ rigth2death/characters/enemies/zombies.py
@@ -1,33 +1,55 @@
+import enum
 import random
-from enum import Enum
+from abc import ABC
 
 from characters.health import Health
-from utils import utils, constants
+from utils import utils
 from utils.custom_sprite import CustomSprite
 
 
-class Zombie:
+class SpritesEnum(enum.Enum):
+    LEFT = 1
+    RIGHT = 2
+    DEATH = 3
 
-    def __init__(self):
-        self.sprite = CustomSprite(utils.img('zombies.png'), 5, is_vertical=False, refresh_time=150)
-        self.death_sprite: CustomSprite = CustomSprite(
-            utils.img('zombie1_death.png'), 7, is_vertical=False, refresh_time=50)
-        self.speed = 2
+
+class Zombie(ABC):
+
+    def __init__(self, speed=1, health=100):
+        self.speed = speed
         self.power = 1
-        self.health = Health()
-
-        self.select_initial_position()
+        self.health = Health(life=health)
         self.selected_strategy = "basic"
         self.last_movements = []
+        self.sprite = None
+        self.death_sprite = None
 
-    def select_initial_position(self):
+    def select_initial_position(self, width, height):
         if random.randrange(0, 2) == 0:
-            self.sprite.x(random.randrange(0, constants.WIDTH))
-            self.sprite.y(random.randrange(0, 2) * constants.HEIGHT)
+            self.sprite.x(random.randrange(0, width))
+            self.sprite.y(random.randrange(0, 2) * height)
         else:
-            self.sprite.x(random.randrange(0, 2) * constants.WIDTH)
-            self.sprite.y(random.randrange(0, constants.HEIGHT))
+            self.sprite.x(random.randrange(0, 2) * width)
+            self.sprite.y(random.randrange(0, height))
 
+    def change_sprite(func):
+        def wrapper(*args, **kwargs):
+            this = args[0]
+            aux_mov_x = this.sprite.x()
+            func(*args, **kwargs)
+            new_x, new_y = this.sprite.x(), this.sprite.y()
+
+            if this.sprite.x() <= aux_mov_x:
+                this.sprite = this.movement_sprites.get(SpritesEnum.RIGHT)
+            else:
+                this.sprite = this.movement_sprites.get(SpritesEnum.LEFT)
+
+            this.sprite.move(new_x, new_y)
+            this.play()
+
+        return wrapper
+
+    @change_sprite
     def move(self, player: CustomSprite, blockers):
 
         if self.selected_strategy == 'basic':
@@ -39,7 +61,6 @@ class Zombie:
                 self.last_move_strategy()
             else:
                 self.selected_strategy = 'basic'
-        self.play()
 
     def last_move_strategy(self):
         previous_move = self.last_movements.pop()
@@ -81,32 +102,60 @@ class Zombie:
         return self.death_sprite.sequence == self.death_sprite.current_image + 1
 
 
-class ZombieType(Enum):
-    NORMAL = 1
-    GREEN = 2
+class NormalZombie(Zombie):
+
+    def __init__(self):
+        super().__init__(speed=2)
+
+        self.death_sprite: CustomSprite = CustomSprite(utils.img('normal_zombie_death.png'),
+                                                       7,
+                                                       is_vertical=True,
+                                                       refresh_time=120)
+        self.movement_sprites = {
+            SpritesEnum.LEFT: CustomSprite(utils.img('normal_zombie.png'), 3, is_vertical=True, refresh_time=150),
+            SpritesEnum.RIGHT: CustomSprite(utils.img('normal_zombie.png'), 3, is_vertical=True, refresh_time=150)
+            .flip(horizontal=True)
+        }
+        self.sprite = self.movement_sprites.get(SpritesEnum.LEFT)
+
+
+class TrollZombie(Zombie):
+
+    def __init__(self):
+        super().__init__(health=200)
+        self.death_sprite: CustomSprite = CustomSprite(utils.img('troll_zombie_death.png'),
+                                                       7,
+                                                       is_vertical=False,
+                                                       refresh_time=50)
+
+        self.movement_sprites = {
+            SpritesEnum.LEFT: CustomSprite(utils.img('troll_zombie.png'), 5, is_vertical=False, refresh_time=150),
+            SpritesEnum.RIGHT: CustomSprite(utils.img('troll_zombie.png'), 5, is_vertical=False, refresh_time=150)
+            .flip(horizontal=True)
+        }
+        self.sprite = self.movement_sprites.get(SpritesEnum.LEFT)
+
+
+class AquaZombie(Zombie):
+
+    def __init__(self):
+        super().__init__(health=500, speed=1)
+        self.death_sprite: CustomSprite = CustomSprite(utils.img('aqua_zombie_death.png'),
+                                                       5,
+                                                       is_vertical=True,
+                                                       refresh_time=120)
+
+        self.movement_sprites = {
+            SpritesEnum.LEFT: CustomSprite(utils.img('aqua_zombie.png'), 4, is_vertical=True, refresh_time=150),
+            SpritesEnum.RIGHT: CustomSprite(utils.img('aqua_zombie.png'), 4, is_vertical=True, refresh_time=150)
+            .flip(horizontal=True)
+        }
+        self.sprite = self.movement_sprites.get(SpritesEnum.LEFT)
 
 
 class ZombieFactory:
+    strategy = {0: NormalZombie, 1: TrollZombie, 2: AquaZombie}
 
     @staticmethod
     def generate():
-        return Zombie()
-
-
-class EnemyGroup:
-
-    def __init__(self, enemies=None):
-        self.size = 0
-        self.list: list[Zombie] = [] if enemies is None else enemies
-
-    def add(self, enemy: Zombie) -> None:
-        self.list.append(enemy)
-        self.size += 1
-
-    def delete(self, enemy: Zombie) -> bool:
-        success: bool = False
-        if enemy in self.list:
-            self.list.remove(enemy)
-            success = True
-            self.size -= 1
-        return success
+        return ZombieFactory.strategy[random.randrange(0, len(ZombieFactory.strategy))]()
